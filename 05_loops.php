3d8539d2f3c0b22b8ad27f83c9ed82b4b02659f3
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-24 11:46:56 -0300

    Loops

---
+++ 05_loops.php
@@ -0,0 +1,40 @@
+<?php
+/* --------- For Loop --------- */
+// for ($x = 0; $x <= 10; $x++) {
+//   echo "$x";
+// }
+
+/* --------- While Loop --------- */
+// $x = 1;
+// while ($x <= 10) {
+//   echo "Number $x <br>";
+//   $x++;
+// }
+
+/* --------- Do...While Loop --------- */
+// $x = 40;
+// do {
+//   echo "Number $x <br>";
+//   $x++;
+// } while ($x <= 10);
+
+/* --------- Foreach Loop --------- */
+// $people = ['Brad', 'Jose', 'William'];
+
+// for ($i = 0; $i < count($people); $i++) {
+//   echo $people[$i] . '<br>';
+// }
+
+// foreach ($people as $index => $person) {
+//   echo $index . ' - ' . $person . '<br>';
+// }
+
+// $person = [
+//   'first_name' => 'Leo',
+//   'last_name' => 'Ribeiro',
+//   'age' => 28,
+// ];
+
+// foreach ($person as $key => $value) {
+//   echo "$key: $value <br>";
+// }
