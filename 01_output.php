badb4d4ed9634cca99d7cbdd0922f3f4a545f430
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2024-01-24 10:37:21 -0300

    Output constructs and functions

---
+++ 01_output.php
@@ -0,0 +1,28 @@
+<?php
+// echo 123, 'Hello', 10.4;
+
+// print 123;
+
+// print_r([1, 2, 3]);
+
+// var_dump(0)
+
+// var_export(2);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>
+    <?php echo 'Post one' ?>
+  </h1>
+</body>
+
+</html>
